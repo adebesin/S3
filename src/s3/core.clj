92d3a993e9f22cb535321d8db39b453c6580b7b8
adebesin <gregory.adebesin@gmail.com>
2018-10-21 18:04:43 +0100

    complete multipart upload

--- src/s3/core.clj
+++ src/s3/core.clj
@@ -1,37 +1,61 @@
 (ns s3.core
   (:import (software.amazon.awssdk.services.s3 S3AsyncClient)
-           (software.amazon.awssdk.services.s3.model AbortMultipartUploadRequest ListMultipartUploadsRequest RequestPayer)
+           (software.amazon.awssdk.services.s3.model AbortMultipartUploadRequest ListMultipartUploadsRequest RequestPayer ListObjectsV2Request CompleteMultipartUploadRequest)
            (software.amazon.awssdk.auth.credentials ProfileCredentialsProvider AwsCredentials AwsCredentialsProvider)))
 
-(defn credentials-provider
+(defn- creds
   [^String name]
   (.build
     (-> (ProfileCredentialsProvider/builder)
         (.profileName name))))
 
-(defn async-client
+(defn- client
   ([]
    (.build
      (S3AsyncClient/builder)))
   ([^String name]
    (.build
      (-> (S3AsyncClient/builder)
-         (.credentialsProvider (credentials-provider name))))))
+         (.credentialsProvider (creds name))))))
 
 (defn abort-multipart-upload
   [& {:keys                                                 ;;TODO add request payer
       [^String bucket
        ^String key
-       upload-id
+       ^String upload-id
+       ^String requester-payer
        ^String profile]
       :or
-      {profile "default"}}]
+      {profile         "default"
+       requester-payer "requester"}}]
   (->>
     (.build
       (->
         (AbortMultipartUploadRequest/builder)
         (.bucket bucket)
         (.key key)
-        (.uploadId upload-id)))
-    (.abortMultipartUpload (async-client profile))))
+        (.uploadId upload-id)
+        (.requestPayer requester-payer)))
+    (.abortMultipartUpload (client profile))))
+
+(defn complete-multipart-upload
+  [& {:keys
+      [^String bucket
+       ^String key
+       ^String upload-id
+       ^String request-payer
+       ^String profile]
+      :or
+      {profile       "default"
+       request-payer "requester"}}]
+  (->>
+    (.build
+      (->
+        (CompleteMultipartUploadRequest/builder)
+        (.bucket bucket)
+        (.key key)
+        (.uploadId upload-id)
+        (.requestPayer request-payer)))
+    (.completeMultipartUpload (client profile))))
+
 
