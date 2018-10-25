598aa9de5890f0cdbd08d2ba1a61563b24f96afa
Adebesin <ldev515@pc58585al.retail2u.trcg.co.uk>
2018-10-25 16:06:22 +0100

    copy

--- src/s3/core.clj
+++ src/s3/core.clj
@@ -5,8 +5,8 @@
              CompleteMultipartUploadRequest
              CopyObjectRequest)
            (software.amazon.awssdk.auth.credentials
-             ProfileCredentialsProvider
-             )))
+             ProfileCredentialsProvider)
+           (java.time Instant)))
 
 (defn- creds
   [^String name]
@@ -24,15 +24,15 @@
          (.credentialsProvider (creds name))))))
 
 (defn abort-multipart-upload
-  [& {:keys
-      [^String bucket
-       ^String key
-       ^String upload-id
-       ^String requester-payer
-       ^String profile]
-      :or
-      {requester-payer "requester"
-       profile         "default"}}]
+  [{:keys
+    [^String bucket
+     ^String key
+     ^String upload-id
+     ^String requester-payer
+     ^String profile]
+    :or
+    {requester-payer "requester"
+     profile         "default"}}]
   (.abortMultipartUpload
     (client profile)
     (.build
@@ -44,15 +44,15 @@
         (.requestPayer requester-payer)))))
 
 (defn complete-multipart-upload
-  [& {:keys
-      [^String bucket
-       ^String key
-       ^String upload-id
-       ^String request-payer
-       ^String profile]
-      :or
-      {request-payer "requester"
-       profile       "default"}}]
+  [{:keys
+    [^String bucket
+     ^String key
+     ^String upload-id
+     ^String request-payer
+     ^String profile]
+    :or
+    {request-payer "requester"
+     profile       "default"}}]
   (.completeMultipartUpload
     (client profile)
     (.build
@@ -63,79 +63,55 @@
         (.uploadId upload-id)
         (.requestPayer request-payer)))))
 
-(defn copy-source
-  [& {:keys
-          [^String copy-source
-           ^String bucket
-           ^String key
-           ^String request-payer
-           ^String profile]
-      :or {request-payer "requester"
-           profile       "default"}}]
+(defmulti copy-object :Type)
+
+(defmethod copy-object :IfMatch
+  [{:keys
+    [^String profile
+     ^String source
+     ^String key
+     ^String bucket
+     ^String request-payer]}]
+  (.copyObject
+    (client (:profile this))
+    (.build
+      (->
+        (CopyObjectRequest/builder)
+        (.copySourceIfMatch source)
+        (.key key)
+        (.bucket bucket)
+        (.requestPayer request-payer)))))
+
+(defmethod copy-object :Source
+  [{:keys
+    [^String profile
+     ^String source
+     ^String key
+     ^String bucket
+     ^String request-payer]}]
   (.copyObject
     (client profile)
     (.build
       (->
         (CopyObjectRequest/builder)
-        (.copySource copy-source)
+        (.copySource source)
         (.key key)
         (.bucket bucket)
         (.requestPayer request-payer)))))
 
-(defn copy-source-if-match
-  [& {:keys
-          [^String copy-source
-           ^String bucket
-           ^String key
-           ^String request-payer
-           ^String profile]
-      :or {request-payer "requester"
-           profile       "default"}}]
+(defmethod copy-object :IfModifiedSince
+  [{:keys
+    [^String profile
+     ^Instant instant
+     ^String bucket
+     ^String request-payer]}]
   (.copyObject
     (client profile)
     (.build
       (->
         (CopyObjectRequest/builder)
-        (.copySourceIfMatch copy-source)
+        (.copySourceIfModifiedSince instant)
         (.key key)
         (.bucket bucket)
         (.requestPayer request-payer)))))
-
-
-(defn copy-object
-  [& {:keys
-          [^String profile
-           ^CopyObjectRequest request]
-      :or {profile "default"}}]
-  (.copyObject (client profile) request))
-
-(defn if-match
-  [& {:keys
-      [^String copy-source
-       ^String bucket
-       ^String key
-       ^String request-payer]
-      :or {request-payer "requester"}}]
-  (.build
-      (->
-        (CopyObjectRequest/builder)
-        (.copySourceIfMatch copy-source)
-        (.key key)
-        (.bucket bucket)
-        (.requestPayer request-payer))))
-
-(defn source
-  [& {:keys
-      [^String copy-source
-       ^String bucket
-       ^String key
-       ^String request-payer]
-      :or {request-payer "requester"}}]
-  (.build
-      (->
-        (CopyObjectRequest/builder)
-        (.copySource copy-source)
-        (.key key)
-        (.bucket bucket)
-        (.requestPayer request-payer))))
 
